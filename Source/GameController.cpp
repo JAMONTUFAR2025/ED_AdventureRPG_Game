f275316864d04d10cfdeadffda3495835f25f862
ACME_DataScraping <https://github.com/JAMONTUFAR2025/ACME_DataScrapping.git>
2025-11-08 22:21:47 -0600

    Initial commit: Add game source, launcher, and project configuration, ignoring Libraries folder.

---
+++ Source/GameController.cpp
@@ -0,0 +1,6 @@
+#include "GameController.h"
+#include <iostream>
+
+void GameController::holaMundo() {
+    std::cout << "hola mundo" << std::endl;
+}
---
+++ Source/GameController.h
@@ -0,0 +1,9 @@
+#ifndef GAMECONTROLLER_H
+#define GAMECONTROLLER_H
+
+class GameController {
+public:
+    void holaMundo();
+};
+
+#endif // GAMECONTROLLER_H
