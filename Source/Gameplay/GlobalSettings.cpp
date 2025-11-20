994eb9a433ad5fba57d7f85e61da9ac4d7bb1207
ACME_DataScraping <https://github.com/JAMONTUFAR2025/ACME_DataScrapping.git>
2025-11-20 12:52:48 -0600

    Se arreglo el .exe

--- Source/Gameplay/GlobalSettings.cpp
+++ Source/Gameplay/GlobalSettings.cpp
@@ -4,4 +4,4 @@
 const unsigned int GlobalSettings::SCREEN_WIDTH = 800;
 const unsigned int GlobalSettings::SCREEN_HEIGHT = 600;
 const int GlobalSettings::FONT_SIZE = 32;
-const string GlobalSettings::FONT_PATH = "Assets/fonts/font.otf";
+const string GlobalSettings::FONT_PATH = "../Assets/fonts/font.otf";
